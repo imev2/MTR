d50a2b42acb223d10f257f42f27ba0ed04000c95
imev2023 <ivalenbreder@gmail.com>
2023-07-27 11:50:14 +0100

    Tensorboard visuals

--- Scripts/plotTensorboard.py
+++ Scripts/plotTensorboard.py
@@ -7,32 +7,40 @@ Created on Wed Jul 26 17:30:29 2023
 """
 import os
 import pandas as pd
-from plotnine import ggplot, aes, geom_line, labs, theme_minimal
+import pandas as pd
+# import seaborn as sns
+import matplotlib.pyplot as plt
+
+# from plotnine import ggplot, aes, geom_line, labs, theme_minimal
 fold = os.getcwd()
 fold
 
 # Read the CSV files into pandas DataFrames
-val_loss_df = pd.read_csv(fold+'/data/ST1/tensorboard/CV1_scale/Loss_validation-tag-scale_modelCV1_2207_1730.csv')
-train_loss_df = pd.read_csv(fold+'/data/ST1/tensorboard/CV1_scale/Loss_train-tag-scale_modelCV1_2207_1730.csv')
-val_accuracy_df = pd.read_csv(fold+'/data/ST1/tensorboard/CV1_scale/Accuracy_validation-tag-scale_modelCV1_2207_1730.csv')
-train_accuracy_df = pd.read_csv(fold+'/data/ST1/tensorboard/CV1_scale/Accuracy_train-tag-scale_modelCV1_2207_1730.csv')
+val_loss_df = pd.read_csv(fold+'/data/ST2/tensorboard/Loss_validation-tag-modelCVsimp_dense__24_07__0001bs16.csv')
+train_loss_df = pd.read_csv(fold+'/data/ST2/tensorboard/Loss_train-tag-modelCVsimp_dense__24_07__0001bs16.csv')
+val_accuracy_df = pd.read_csv(fold+'/data/ST2/tensorboard/Accuracy_validation-tag-modelCVsimp_dense__24_07__0001bs16.csv')
+train_accuracy_df = pd.read_csv(fold+'/data/ST2/tensorboard/Accuracy_train-tag-modelCVsimp_dense__24_07__0001bs16.csv')
 
 # Merge the DataFrames based on the "Step" column
 merged_df = val_loss_df.merge(train_loss_df, on='Step', suffixes=('_val_loss', '_train_loss'))
 merged_df = merged_df.merge(val_accuracy_df, on='Step', suffixes=('_val_loss', '_val_accuracy'))
 merged_df = merged_df.merge(train_accuracy_df, on='Step', suffixes=('_val_accuracy', '_train_accuracy'))
 
-# Plot using plotnine
-plot = ggplot(merged_df, aes(x='Step')) + theme_minimal()
+# Plot using seaborn and matplotlib
+plt.figure(figsize=(10, 6))
 
 # Add the curves for each CSV file
-plot += geom_line(aes(y='Value_val_loss'), color='blue', size=1, linetype='solid')
-plot += geom_line(aes(y='Value_train_loss'), color='red', size=1, linetype='dashed')
-plot += geom_line(aes(y='Value_val_accuracy'), color='green', size=1, linetype='solid')
-plot += geom_line(aes(y='Value_train_accuracy'), color='orange', size=1, linetype='dashed')
+plt.plot(merged_df['Step'], merged_df['Value_val_loss'], label='Validation Loss', color='darkred', linestyle='solid')
+plt.plot(merged_df['Step'], merged_df['Value_val_accuracy'], label='Validation Accuracy', color='pink', linestyle='solid')
+plt.plot(merged_df['Step'], merged_df['Value_train_loss'], label='Train Loss', color='darkblue', linestyle='solid')
+plt.plot(merged_df['Step'], merged_df['Value_train_accuracy'], label='Train Accuracy', color='lightblue', linestyle='solid')
 
 # Add labels and title
-plot += labs(x='Step', y='Value', title='Change in Value at Each Step')
+plt.xlabel('Step')
+plt.ylabel('Value')
+plt.title('UMAP-Grid')
+plt.legend()
 
 # Display the plot
-print(plot)
+plt.show()
+
